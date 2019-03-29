5c4bdf7b588ff66715010410fef597c62e7f5a5d
Stepan <n.stepan.97@mail.ru>
2019-03-29 11:32:33 +0400

    first commit

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+
+  data: Array<object> = null;
+
+  constructor() { 
+  }
+
+  ngOnInit() {
+    this.getDataFromServer();
+  }
+
+  getDataFromServer(){
+    fetch('http://localhost:3000/users')
+      .then(async (response) => {
+        const data = await response.json();
+        if(response.status === 200 && data){
+          this.data = data;
+        }else {
+          console.log("Could not get data from server...") ;
+        }
+      });
+  }
+}
